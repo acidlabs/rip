727603eebcef8011518ddfa8aefee911ecb861d5
vladiloquai <vladimir_suarez@hotmail.com>
2012-07-10 16:54:09 -0300

    Update master

--- src/rip/middleware.clj
+++ src/rip/middleware.clj
@@ -19,12 +19,12 @@
 
 (defn- json-request?
   [req]
-  (if-let [#^String type (get-in req [:headers "Content-Type"])]
+  (if-let [#^String type (get-in req [:headers "content-type"])]
     (.startsWith type "application/json")))
 
 (defn- xml-request?
   [req]
-  (if-let [#^String type (get-in req [:headers "Content-Type"])]
+  (if-let [#^String type (get-in req [:headers "content-type"])]
     (.startsWith type "application/xml")))
 
 (defn xml->hash-map
